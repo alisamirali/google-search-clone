73c3e87919f524a0acfe9fd92c49139f4a5c8522
Ali Samir <alisamir.eng@gmail.com>
2023-05-21 21:41:58 +0300

    Home Page - Search Header - Search Options

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+import { AiFillCamera } from "react-icons/ai";
 
 const HomeSearch = () => {
-  return <div>HomeSearch</div>;
+  const router = useRouter();
+  const [input, setInput] = useState("");
+  const [randomSearchLoading, setRandomSearchLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!input.trim()) return;
+
+    router.push(`/search/web?searchTerm=${input}`);
+  }
+
+  async function randomSearch() {
+    setRandomSearchLoading(true);
+
+    const response = await fetch("https://random-word-api.herokuapp.com/word")
+      .then((res) => res.json())
+      .then((data) => data[0]);
+
+    if (!response) return;
+
+    router.push(`/search/web?searchTerm=${response}`);
+    setRandomSearchLoading(false);
+  }
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+      >
+        <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
+        <input
+          type="text"
+          autoFocus={true}
+          className="flex-grow focus:outline-none bg-transparent"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <div className="flex items-center justify-center gap-3">
+          <BsFillMicFill className="text-lg" />
+          <AiFillCamera className="text-xl" />
+        </div>
+      </form>
+
+      <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8 ">
+        <button onClick={handleSubmit} className="btn">
+          Google Search
+        </button>
+        <button
+          disabled={randomSearchLoading}
+          onClick={randomSearch}
+          className="btn flex items-center justify-center disabled:opacity-80"
+        >
+          {randomSearchLoading ? (
+            <img
+              src="spinner.svg"
+              alt="loading..."
+              className="h-6 text-center"
+            />
+          ) : (
+            "I am Feeling Lucky"
+          )}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default HomeSearch;
